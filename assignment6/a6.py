dae49dfd17ab8735159faba9e06dbf140c38c563
giganizer <ziga.bradas@gmail.com>
2024-01-23 18:16:11 +0100

    Assignment 6 solution (all tasks working; pictures of my face not commited)

---
+++ assignment6/a6.py
@@ -0,0 +1,380 @@
+# %% [markdown]
+# ## Assignment 6: Reduction of dimensionality and recognition
+# Machine perception<br>
+# 2023/2024
+
+# %%
+import numpy as np
+import a6_utils as a6
+import cv2
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+import matplotlib.patches as patches
+
+# %% [markdown]
+# ### Exercise 1: Direct PCA method
+
+# %% [markdown]
+# #### (a)
+
+# %%
+# na papirju
+
+# %% [markdown]
+# #### (b)
+
+# %%
+def pca2d_b(filename):
+    points = np.loadtxt(filename)
+    X = points.T
+    mi = np.mean(X, axis=1)
+    Xd = X - mi[:, np.newaxis]
+    C = 1/(len(points)-1) * np.dot(Xd, Xd.T)
+    U, S, V = np.linalg.svd(C)
+
+    plt.scatter(points[:,0], points[:,1])
+    a6.drawEllipse(mi, C, 1)
+    plt.show()
+
+# %%
+filename = "data/points.txt"
+pca2d_b(filename)
+
+# %% [markdown]
+# #### (c)
+
+# %%
+def pca2d_c(filename):
+    points = np.loadtxt(filename)
+    X = points.T
+    mi = np.mean(X, axis=1)
+    Xd = X - mi[:, np.newaxis]
+    C = 1/(len(points)-1) * np.dot(Xd, Xd.T)
+    U, S, V = np.linalg.svd(C)
+
+    plt.scatter(points[:,0], points[:,1])
+    a6.drawEllipse(mi, C, 1)
+
+    evcs = [U[:,i] for i in range(U.shape[1])]
+    evls = [S[i] for i in range(len(S))]
+    evcs = [evcs[i]*np.sqrt(evls[i]) for i in range(len(evcs))]
+
+    hardcoded_colors = ("red", "green")
+    for i in range(len(evcs)):
+        plt.plot([mi[0], mi[0]+evcs[i][0]], [mi[1], mi[1]+evcs[i][1]], color=hardcoded_colors[i])
+    
+    plt.show()
+    return evcs, evls
+
+# %%
+e1eigenvecs, e1eigenvals = pca2d_c(filename)
+
+# %% [markdown]
+# #### (d)
+
+# %%
+e1cs = np.cumsum(e1eigenvals)
+e1cs = e1cs / np.max(e1cs)
+
+plt.bar(range(len(e1cs)), e1cs, width=0.5)
+plt.show()
+
+# %%
+print("Percent of information retained when discarding the second eigenvector:", round(e1cs[0],3)*100)
+
+# %% [markdown]
+# #### (e)
+
+# %%
+# def pca2d_c(filename):
+points = np.loadtxt(filename)
+X = points.T
+mi = np.mean(X, axis=1)
+Xd = X - mi[:, np.newaxis]
+C = 1/(len(points)-1) * np.dot(Xd, Xd.T)
+U, S, V = np.linalg.svd(C)
+
+plt.scatter(points[:,0], points[:,1])
+a6.drawEllipse(mi, C, 1)
+
+evcs = [U[:,i] for i in range(U.shape[1])]
+evls = [S[i] for i in range(len(S))]
+evcs = [evcs[i]*np.sqrt(evls[i]) for i in range(len(evcs))]
+
+hardcoded_colors = ("red", "green")
+for i in range(len(evcs)):
+    plt.plot([mi[0], mi[0]+evcs[i][0]], [mi[1], mi[1]+evcs[i][1]], color=hardcoded_colors[i])
+
+yd = np.dot(U.T, X-mi[:, np.newaxis])
+U[:, 1] = 0 #second eigenvector -> lowest variance
+e1xq = np.dot(U, yd) + mi[:, np.newaxis]
+
+plt.scatter(e1xq[0], e1xq[1])
+
+plt.show()
+
+# %% [markdown]
+# #### (f)
+
+# %%
+qpoint=np.array((6, 6))
+qpDist = []
+for p in points:
+    qpDist.append(np.linalg.norm(qpoint - p))
+e1closest = np.argmin(qpDist) #index of closest point
+plt.scatter(qpoint[0], qpoint[1], color="purple")
+
+points = np.loadtxt(filename)
+X = points.T
+mi = np.mean(X, axis=1)
+Xd = X - mi[:, np.newaxis]
+C = 1/(len(points)-1) * np.dot(Xd, Xd.T)
+U, S, V = np.linalg.svd(C)
+
+plt.scatter(points[:,0], points[:,1])
+a6.drawEllipse(mi, C, 1)
+
+evcs = [U[:,i] for i in range(U.shape[1])]
+evls = [S[i] for i in range(len(S))]
+evcs = [evcs[i]*np.sqrt(evls[i]) for i in range(len(evcs))]
+
+hardcoded_colors = ("red", "green")
+for i in range(len(evcs)):
+    plt.plot([mi[0], mi[0]+evcs[i][0]], [mi[1], mi[1]+evcs[i][1]], color=hardcoded_colors[i])
+
+newX = np.concatenate((X,qpoint.reshape((2,1))), axis=1)
+yd = np.dot(U.T, newX-mi[:, np.newaxis])
+U[:, 1] = 0 #second eigenvector -> lowest variance
+e1xq2 = np.dot(U, yd) + mi[:, np.newaxis]
+
+plt.scatter(e1xq2[0,:-1], e1xq2[1,:-1])
+plt.scatter(e1xq2[0,-1], e1xq2[1,-1], color="indigo")
+
+qpointAfter = e1xq2[:,-1]
+qpDistAfter = []
+for p in e1xq2.T[:-1]:
+    qpDistAfter.append(np.linalg.norm(qpointAfter - p))
+e1closestAfter = np.argmin(qpDistAfter) #index of closest point
+
+circle = patches.Circle((points[e1closest][0], points[e1closest][1]), 0.2, edgecolor='blue', facecolor='none', linewidth=1)
+plt.gca().add_patch(circle)
+circle = patches.Circle((e1xq2.T[e1closestAfter][0], e1xq2.T[e1closestAfter][1]), 0.2, edgecolor='red', facecolor='none', linewidth=1)
+plt.gca().add_patch(circle)
+
+plt.show()
+
+# %%
+print("Closest point before:", points[e1closest])
+print("Closest point after:", e1xq2.T[e1closestAfter])
+
+# %% [markdown]
+# ### Exercise 2: The dual PCA method
+
+# %% [markdown]
+# #### (a)
+
+# %%
+points = np.loadtxt(filename)
+X = points.T
+mi = np.mean(X, axis=1)
+Xd = X - mi[:, np.newaxis]
+C2 = 1/(len(points)-1) * np.dot(Xd.T, Xd) #this is now different
+U, S, V = np.linalg.svd(C2)
+U = np.dot(np.dot(Xd, U), np.diag(np.sqrt(1/(S * (len(points)-1)))))
+
+
+plt.scatter(points[:,0], points[:,1])
+a6.drawEllipse(mi, C, 1)
+
+evcs = [U[:,i] for i in range(2)]
+evls = [S[i] for i in range(2)]
+evcs = [evcs[i]*np.sqrt(evls[i]) for i in range(len(evcs))]
+
+hardcoded_colors = ("red", "green")
+for i in range(len(evcs)):
+    plt.plot([mi[0], mi[0]+evcs[i][0]], [mi[1], mi[1]+evcs[i][1]], color=hardcoded_colors[i])
+
+plt.show()
+
+# %%
+print(U)
+U[np.abs(U) < 1e-7] = 0
+print(U)
+
+# %% [markdown]
+# #### (b)
+
+# %%
+points = np.loadtxt(filename)
+X = points.T
+mi = np.mean(X, axis=1)
+Xd = X - mi[:, np.newaxis]
+C2 = 1/(len(points)-1) * np.dot(Xd.T, Xd) #this is now different
+U, S, V = np.linalg.svd(C2)
+U = np.dot(np.dot(Xd, U), np.diag(np.sqrt(1/(S * (len(points)-1)))))
+U[np.abs(U) < 1e-7] = 0
+
+plt.scatter(points[:,0], points[:,1])
+a6.drawEllipse(mi, C, 1)
+
+evcs = [U[:,i] for i in range(2)]
+evls = [S[i] for i in range(2)]
+evcs = [evcs[i]*np.sqrt(evls[i]) for i in range(len(evcs))]
+
+hardcoded_colors = ("red", "green")
+for i in range(len(evcs)):
+    plt.plot([mi[0], mi[0]+evcs[i][0]], [mi[1], mi[1]+evcs[i][1]], color=hardcoded_colors[i])
+
+yd = np.dot(U.T, X-mi[:, np.newaxis])
+U[:, 1] = 0 #second eigenvector -> lowest variance
+e2xq = np.dot(U, yd) + mi[:, np.newaxis]
+
+plt.scatter(e2xq[0], e2xq[1])
+
+plt.show()
+
+# %%
+e1xq - e2xq
+
+# %% [markdown]
+# ### Exercise 3: Image decomposition examples
+
+# %% [markdown]
+# #### (a)
+
+# %%
+series = 1
+faces_images = []
+for i in range(1,65):
+    img = cv2.imread("data/faces/%d/%03d.png" % (series, i))
+    if img is None: raise ValueError
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    faces_images.append(img)
+m, n = faces_images[0].shape
+
+# %%
+matrix = np.zeros((m*n, 64))
+for i in range(64):
+    matrix[:,i] = faces_images[i].flatten()
+
+# %% [markdown]
+# #### (b)
+
+# %%
+def dualPcaImgs(matrix):
+    X = matrix
+    N = len(matrix[0])
+    mi = np.mean(X, axis=1)
+    Xd = X - mi[:, np.newaxis]
+    C = 1/(N-1) * np.dot(Xd.T, Xd)
+    U, S, V = np.linalg.svd(C)
+    S = S + 10e-15
+    U = np.dot(np.dot(Xd, U), np.diag(np.sqrt(1/(S * (N-1)))))
+    #U[np.abs(U) < 1e-7] = 0
+    return U, mi
+
+# %%
+e3evcs, e3mean = dualPcaImgs(matrix)
+
+first5 = []
+for i in range(5):
+    img = np.reshape(e3evcs[:,i], (m, n))
+    first5.append(img)
+
+# %%
+plt.figure(figsize=(15,3))
+for i in range(5):
+    plt.subplot(1,5,i+1)
+    plt.imshow(first5[i],cmap="gray")
+plt.show()
+
+# %%
+img1 = faces_images[0].flatten()
+
+toPca1 = np.dot(e3evcs.T, (img1 - e3mean))
+back1 = np.dot(e3evcs, toPca1) + e3mean
+
+dimDelImg = np.copy(img1)
+dimDelImg[4078] = 0
+
+toPca2 = np.dot(e3evcs.T, (img1 - e3mean))
+toPca2[0] = 0
+back2 = np.dot(e3evcs, toPca2) + e3mean
+toPca3 = np.dot(e3evcs.T, (img1 - e3mean))
+toPca3[1] = 0
+back3 = np.dot(e3evcs, toPca3) + e3mean
+
+# %%
+toPca2[:5]
+
+# %%
+img1 = img1.reshape((96,84))
+back1 = back1.reshape((96,84))
+dimDelImg = dimDelImg.reshape((96,84))
+back2 = back2.reshape((96,84))
+back3 = back3.reshape((96,84))
+
+plt.figure(figsize=(14,14))
+spl_rows, spl_cols = 4, 3
+plt.subplot(spl_rows, spl_cols,1)
+plt.imshow(img1, cmap="gray")
+plt.title("Original")
+plt.subplot(spl_rows, spl_cols,4)
+plt.imshow(img1, cmap="gray")
+plt.title("Original")
+plt.subplot(spl_rows, spl_cols,7)
+plt.imshow(img1, cmap="gray")
+plt.title("Original")
+plt.subplot(spl_rows, spl_cols,2)
+plt.imshow(back1, cmap="gray")
+plt.title("Projection to PCA and back")
+plt.subplot(spl_rows, spl_cols,5)
+plt.imshow(dimDelImg, cmap="gray")
+plt.title("Dimension in image space to 0")
+plt.subplot(spl_rows, spl_cols,8)
+plt.imshow(back2, cmap="gray")
+plt.title("Dimension in PCA space to 0")
+plt.subplot(spl_rows, spl_cols,3)
+plt.imshow(img1 - back1, cmap="gray")
+plt.title("Difference")
+plt.subplot(spl_rows, spl_cols,6)
+plt.imshow(img1 - dimDelImg, cmap="gray")
+plt.title("Difference")
+plt.subplot(spl_rows, spl_cols,9)
+plt.imshow(img1 - back2, cmap="gray")
+plt.title("Difference")
+plt.subplot(spl_rows, spl_cols,10)
+plt.imshow(img1, cmap="gray")
+plt.title("Original")
+plt.subplot(spl_rows, spl_cols,11)
+plt.imshow(back3, cmap="gray")
+plt.title("More important dim in PCA to 0")
+plt.subplot(spl_rows, spl_cols,12)
+plt.imshow(img1 - back3, cmap="gray")
+plt.title("Difference")
+plt.show()
+
+# %% [markdown]
+# #### (c)
+
+# %%
+retain = (32, 16, 8, 4, 2, 1)
+random_img = faces_images[np.random.randint(1, 65)].flatten()
+randBacks = []
+for r in retain:
+    randToPca = np.dot(e3evcs.T, (random_img - e3mean))
+    # print(randToPca)
+    randToPca[r::] = 0
+    # print(randToPca)
+    randBack = np.dot(e3evcs, randToPca) + e3mean
+    randBacks.append(randBack.reshape((96,84)))
+
+# %%
+plt.figure(figsize=(15,5))
+for i in range(len(retain)):
+    plt.subplot(1,len(retain),i+1)
+    plt.title(retain[i])
+    plt.imshow(randBacks[i], cmap="gray")
+
+
